f5efdb64836bb3e7a826fd7bd9a453e4a538ee96
Mallikarjuna Reddy Basireddy <mreddy@opentext.com>
2018-11-24 15:06:03 +0530

    adding first

---
+++ Angular5-Seed-master/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <div class="container">
+         Welcome to angular...
+         <dash-comp></dash-comp>
+        </div>
+    `
+})
+export class AppComponent { }
---
+++ Angular5-Seed-master/src/app/dashboard.component.ts
@@ -0,0 +1,13 @@
+ import { Component} from '@angular/core';
+
+@Component({
+    selector: 'dash-comp',
+    template: `<div> 
+                <h2> Displaying Dashboard </h2></div>
+    
+    `
+})
+
+export class DashboardComponent{
+   
+}
